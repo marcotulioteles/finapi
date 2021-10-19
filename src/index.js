06bd1c044b544bf4831b2a2eb47df370e7eaf4a8
Marco Túlio Teles <marcotuliocivileng@gmail.com>
2021-10-18 23:45:12 -0300

    created first routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const { v4: uuiv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Hello World Ignite!"
+let customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({error: "Customer already exists!"})
+  }
+  
+  customers.push({
+    cpf,
+    name,
+    id: uuiv4(),
+    statement: []
   });
+
+  return response.status(201).send();
+});
+
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({error: "Customer not found!"});
+  }
+
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
