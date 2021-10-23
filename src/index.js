483222ec7b6256be6f3d3654449c21bf30ad4022
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2021-10-23 13:26:21 -0300

    created withdraw bank account route

--- src/index.js
+++ src/index.js
@@ -18,7 +18,19 @@ function verifyIfExistsAccountCPF(request, response, next) {
   request.customer = customer;
 
   return next();
-}
+};
+
+function getBalance(statement) {
+  const balance = statement.reduce((accumulator, operation) => {
+    if (operation.type === 'credit') {
+      return accumulator + operation.amount;
+    } else {
+      return accumulator - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+};
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
@@ -59,6 +71,28 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
   }
 
   customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
+
+  const statementOperations = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  }
+
+  customer.statement.push(statementOperations);
 
   return response.status(201).send();
 });
