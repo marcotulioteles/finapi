d8675082a55b18aaca6217b3e0fb4a56f003c61a
Marco Tulio Teles <marcotuliocivileng@gmail.com>
2021-10-23 15:14:09 -0300

    created Delete Account route

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const { v4: uuiv4 } = require('uuid');
 
@@ -95,6 +96,49 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
   customer.statement.push(statementOperations);
 
   return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormatted = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(statement => statement.created_at
+    .toDateString() === new Date(dateFormatted).toDateString());
+
+  return response.json(statement);
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
 });
 
 app.listen(3333);
